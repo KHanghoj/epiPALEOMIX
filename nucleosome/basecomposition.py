9d5a67346301beb3c2662d827f01999d57e5c487
hullibulli <kristianhanghoej@gmail.com>
2014-11-12 14:07:25 +0100

    added basecomposition.py file

---
+++ nucleosome/basecomposition.py
@@ -0,0 +1,4 @@
+#!/opt/local/bin/python
+'''  Object: return the base composition of the bases in the called nucleosome.
+Single-, di- and tetranucleotides '''
+from __future__ import print_function
