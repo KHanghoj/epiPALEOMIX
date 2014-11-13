7d3a5f41e3ba446704ad2840fa08db9032c1c54b
hullibulli <kristianhanghoej@gmail.com>
2014-11-13 16:40:32 +0100

    script to make fasta snippets for testing

---
+++ trash/getfastsnippet.py
@@ -0,0 +1,19 @@
+''' to get fasta.fa.fai: samtools faidx fasta.fa
+This script creates a million bases from 10,000,000 to 11,000,000
+of any given fasta file. in this case hs.build37 as the Labrana and saqqaq
+is aligned against that build
+'''
+
+
+import pysam, sys
+f = sys.argv[1]
+chrom = sys.argv[2]
+
+x = pysam.Fastafile(f)
+
+fasta_snip = open('fasta_snip.fa', 'w')
+y = x.fetch(chrom, start=10000000, end=11000000)
+fasta_snip.write(y)
+x.close()
+fasta_snip.close()
+
