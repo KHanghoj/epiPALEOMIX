bcdac771358cf669d32a5a09f09a275f25121f3f
hullibulli <kristianhanghoej@gmail.com>
2014-11-18 18:43:37 +0100

    sdf

--- methylation/methyl_fetch.py
+++ methylation/methyl_fetch.py
@@ -1,7 +1,8 @@
 #!/opt/local/bin/python
 '''  Object: To find the methylation value from a region. the methylation
 score (Ms)
-python ~/research/projects/epiomix/methylation/methyl_fetch.py fasta_snip.fa test.bam --chrom 22 --start 18100000 --end 19100000 --out new.txt
+ python ~/research/projects/epiomix/methylation/methyl_fetch.py chrome.fa test.bam --chrom 22 --start 18100000 --end 20100000 --out new.txt
+ Rscript -e "a=read.table('new.txt') ;summary(a)"
         # Profiling af python script:
         # $ python -m cProfile -s cumulative script.py
 
@@ -35,46 +36,43 @@ def main(argv):
     samfile = pysam.Samfile(args.bam, "rb")
     fasta = pysam.Fastafile(args.fastafile)
 
-
     f_output = open(args.out, 'w')  # the output file
-    # f_output.write('\t'.join(headers)+'\n')
 
     last_pos = -1
     chrom = ''
-    scores = {}
-    # for record in samfile.fetch(args.chrom, args.start, args.end):
-    #     for gammel_pos in xrange(last_pos, record.pos):
-    #         statistikker = scores.pop(gammel_pos, None)
-    #         if statistikker:
-    #             # print Ms
+
     dic_lastpos = defaultdict(lambda: defaultdict(int))
     dic_base_forward = defaultdict(int)
-    # dic_base_end = defaultdict(int)
-    # dic_lastpos = {}
-
 
     # mybeds = read.beds(...)
     # handle = pysam.samfile(...)
     # for bed in mybeds:
     #    call_Ms(handle, bed)
 
+# put stuff into function
+# compare perforance with former script
+# see if i can avoid the keys thing
+# i get    2578 lines in new.txt when len(dic_lastpos.keys()) >= 2
+# i get     2513 lines in new.txt len(dic_lastpos.keys()) >= 200
+
+
     for record in samfile.fetch(args.chrom, args.start, args.end):
         read_sequence = record.seq
         read_cigar = record.cigar
 
-        if len(dic_lastpos) >= 10 and (max(dic_lastpos) < last_pos):
+        if len(dic_lastpos.keys()) >= 1 and (max(dic_lastpos.keys()) < last_pos): # is this keys thing a hack???
             for keys in sorted(dic_lastpos.keys()):
                 if last_pos > keys:
                     top = dic_lastpos[keys]['A']
                     bottom = dic_lastpos[keys]['G']+dic_lastpos[keys]['A']
                     out = top/float(bottom)
                     print(samfile.getrname(record.tid), keys+1, out,
-                          file=f_output)
+                          file=f_output,sep='\t')
                     dic_lastpos.pop(keys, None)
         if record.tid != chrom:  # new chromosome
             chrom = record.tid
             last_pos = -1
-        
+
         if record.is_reverse:
             if read_sequence[-2:] in _MINUS_STRAND_BASES:  # last two bases ok
                 fetch_posi = record.aend-2
@@ -84,7 +82,6 @@ def main(argv):
                     if (cigar_op == 0) and (cigar_len >= 2):
                         dic_lastpos[record.aend-2][read_sequence[-1]] += 1
 
-
         else:  # this is for the forward strand
             if read_sequence[:2] in _PLUS_STRAND_BASES:
                 fetch_posi = record.pos
@@ -92,22 +89,22 @@ def main(argv):
                                        start=fetch_posi, end=fetch_posi+2):
 
                     cigar_op, cigar_len = read_cigar[0]
-                    if cigar_op == 0 and cigar_len >= 2:
+                    if (cigar_op) == 0 and (cigar_len) >= 2:
 
                         if record.pos != last_pos and last_pos != -1:
                             # tempdic_minus = dic_lastpos.pop(last_pos, {}) # this is mikkel stuff. see if it works the same
                             # top = dic_base_forward['T']+tempdic_minus.get('A', 0)
                             # lower = top+dic_base_forward['C']+tempdic_minus.get('G', 0)
                             tempdic_minus = dic_lastpos[last_pos]
-                            if tempdic_minus['A'] != 0: print(last_pos, tempdic_minus['A'], 'a')
-                            if tempdic_minus['G'] != 0: print(last_pos, tempdic_minus['G'], 'g') # this happened once ;)
-                            
+                            # if tempdic_minus['A'] != 0: print(last_pos, tempdic_minus['A'], 'a')
+                            # if tempdic_minus['G'] != 0: print(last_pos, tempdic_minus['G'], 'g')
+
                             top = dic_base_forward['T']+tempdic_minus['A']
                             lower = top+dic_base_forward['C']+tempdic_minus['G']
 
                             M_value = top/float(lower)
                             print(samfile.getrname(record.tid),
-                                  last_pos+1, M_value, file=f_output)
+                                  last_pos+1, M_value, file=f_output, sep='\t')
                             dic_base_forward.clear()
                             dic_lastpos.pop(last_pos, None)
                                 # returns none of last_pos key is not present
@@ -122,7 +119,6 @@ def main(argv):
     samfile.close()
     fasta.close()
     return 0
-
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
