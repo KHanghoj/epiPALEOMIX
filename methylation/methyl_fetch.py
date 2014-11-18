f84feafbbcb15cbf15a246c7c65924535167e405
hullibulli <kristianhanghoej@gmail.com>
2014-11-18 14:39:08 +0100

    update of methyl_fetch.py

--- methylation/methyl_fetch.py
+++ methylation/methyl_fetch.py
@@ -8,6 +8,10 @@ from __future__ import print_function
 import sys
 import pysam
 import argparse
+from collections import defaultdict
+
+_PLUS_STRAND_BASES = ['CG', 'TG']
+_MINUS_STRAND_BASES = ['CG', 'CA']
 
 
 def parse_args(argv):
@@ -35,45 +39,115 @@ def main(argv):
 
     last_pos = -1
     scores = {}
+    # for record in samfile.fetch(args.chrom, args.start, args.end):
+    #     for gammel_pos in xrange(last_pos, record.pos):
+    #         statistikker = scores.pop(gammel_pos, None)
+    #         if statistikker:
+    #             # print Ms
+
+
+    dic_base_forward = defaultdict(int)
+    dic_base_end = defaultdict(int)
+    dic_lastpos = defaultdict(int)
+
     for record in samfile.fetch(args.chrom, args.start, args.end):
-        for gammel_pos in xrange(last_pos, record.pos):
-            statistikker = scores.pop(gammel_pos, None)
-            if statistikker:
-                # print Ms
+        read_sequence = record.seq
+        read_cigar = record.cigar
+        if record.pos in dic_lastpos:
+            dic_lastpos[record.pos]
+            pass
+        # read_alignment_end = record.aend
+        # if len(record.cigar)>1 :
+        #     print(read_cigar)
+        #     print(read_sequence)
+        #     print(len(read_sequence))
+        #     print(read_postions)
+        #     print(read_alignment_end)
+
+        #     cigar_oper_start = read_cigar[0][0]
+        #     cigarlength_start = read_cigar[0][1]
 
 
+        #     # this i need for minus strand: combine it with the record.aend
+        #     cigar_oper_end = read_cigar[-1][0]
+        #     cigarlength_end = read_cigar[-1][1]
 
+        #     if(cigar_oper_end is 0 and cigarlength_end >= 2):
+        #         print(read_cigar)
 
+        #     # sys.exit(1)
+        # if record.pos + len(read_sequence) != record.aend:
+        #     print(record.cigar)
+        #     print(record.pos)
+        #     print(record.alen)
+        #     print(record.aend, 'aend')
+        #     print(len(read_sequence))
+        #     sys.exit()
+        # continue
+        if record.is_reverse:
+            if read_sequence[-2:] in _MINUS_STRAND_BASES:  # last two bases ok
+                fetch_posi = record.aend-1
+                if 'CG' in fasta.fetch(samfile.getrname(record.tid),
+                                       start=fetch_posi, end=fetch_posi+2):
+                    if read_cigar[-1][0] == 0 and read_cigar[-1][1] >= 2:
+                        dic_lastpos[record.aend][read_sequence[-1]] += 1
 
+                        # if record.aend == last_pos:
 
+                        #     dic_base_end[read_sequence[-1]] += 1
+                        # else:
+                        #     dic_lastpos[record.aend] = dic_base_end
+            # this is the minus strand
 
-    for record in samfile.fetch(args.chrom, args.start, args.end):
+        else:
+            if read_sequence[0:2] in _PLUS_STRAND_BASES:
+                fetch_posi = record.pos+1
+                if 'CG' in fasta.fetch(samfile.getrname(record.tid),
+                                       start=fetch_posi, end=fetch_posi+2):
+                    if read_cigar[0][0] == 0 and read_cigar[0][1] >= 2:
+                    # if read_sequence[0:2] in _PLUS_STRAND_BASES and \
+                    #         (read_cigar[0][0] == 0 and read_cigar[0][1] > 2):
+                        if record.pos == last_pos:
+                            dic_base_forward[read_sequence[0]] += 1
+                            # count with defaultdic if T (N2) or C (N1)
+                            pass
+                        else:
+                            tempdic_minus = dic_lastpos[last_pos]  # M1 M2
+                            top = sum(dic_base_forward['T'], tempdic_minus['A'])
+                            lower = sum(top, dic_base_forward['C'], tempdic_minus['G'])
+                            M_value = top/float(lower)
+                            print(M_value)
+                            dic_lastpos.pop(last_pos, None)
+                                # returns none of last_pos key is not present
+                            dic_base_forward.clear()
+
         # Sekvens = record.seq
         # Alignment = sekvens + cigar
 
 
+        last_pos = record.pos
         # Starter med:
           # slut_positioner = {}
 
         # Hvis + streng
 
-            # 1. Er de første 2 baser TpG eller CpG i sekvensen (.seq)?
+            # 1. Er de foerste 2 baser TpG eller CpG i sekvensen (.seq)?
 
             # 2. Er de to baser i FASTA filen CpG i alignment?
 
-            # 3. Er de første 2 baser TpG eller CpG i alignment?
+            # 3. Er de foerste 2 baser TpG eller CpG i alignment?
 
             # 3. Samme position som sidste record, eller ny position?
 
-            # 3a. Samme pos = tæl det som N1 eller N2
+            # 3a. Samme pos = tael det som N1 eller N2
 
-            # 3b. Anden pos = Udregne Ms for sidste pos; nulstil tælling; 3a.
+            # 3b. Anden pos = Udregne Ms for sidste pos; nulstil taelling; 3a.
             #                 Hent M1/M2 fra slut_positioner:
             #                   slut_positioner.pop(last_pos, [0, 0])
 
         # Hvis - streng
 
-            # 1. Er de første 2 baser CpG eller CpA i sekvensen (.seq)?
+            # 1. Er de foerste 2 baser CpG eller CpA i sekvensen (.seq)?
 
             # 2. Udregne slut-position via cigar-streng
 
@@ -81,11 +155,11 @@ def main(argv):
 
             # 4. Er de sidste 2 baser CpA eller CpG?
 
-            # 5. Henter vores [M1, M2] fra slut_positioner med pos som nøgle
+            # 5. Henter vores [M1, M2] fra slut_positioner med pos som noegle
 
             # 6. Incrementer M1 eller M2
 
-        # Når ikke flere records; udregn Ms for sidste position
+        # Naar ikke flere records; udregn Ms for sidste position
 
 
 
@@ -107,47 +181,47 @@ def main(argv):
 
 
 
-        posi_fetch = pileupcolumn.pos-1
+        # posi_fetch = pileupcolumn.pos-1
 
-        if not 'CG' in fasta.fetch(args.chrom, start=posi_fetch,
-                                   end=posi_fetch+2):
-                # fetch the two bases of interest from the fasta file
-            continue
-        not_changed = 0
-        C_to_T = 0
-        C_to_other = 0
-        tot_CG = 0
+        # if not 'CG' in fasta.fetch(args.chrom, start=posi_fetch,
+        #                            end=posi_fetch+2):
+        #         # fetch the two bases of interest from the fasta file
+        #     continue
+        # not_changed = 0
+        # C_to_T = 0
+        # C_to_other = 0
+        # tot_CG = 0
 
-        for pileupread in pileupcolumn.pileups:
-            if pileupread.alignment.is_reverse:  # check strand read
-                if (pileupread.qpos == pileupread.alignment.alen-1) and  \
-                        pileupread.alignment.seq[pileupread.qpos-1] == 'C' \
-                        and (not pileupread.indel):  # read on the rev strand
+        # for pileupread in pileupcolumn.pileups:
+        #     if pileupread.alignment.is_reverse:  # check strand read
+        #         if (pileupread.qpos == pileupread.alignment.alen-1) and  \
+        #                 pileupread.alignment.seq[pileupread.qpos-1] == 'C' \
+        #                 and (not pileupread.indel):  # read on the rev strand
 
-                    tot_CG += 1
-                    if pileupread.alignment.seq[pileupread.qpos] == 'A':
-                        C_to_T += 1
-                    elif pileupread.alignment.seq[pileupread.qpos] == 'G':
-                        not_changed += 1
-                    else:
-                        C_to_other += 1
+        #             tot_CG += 1
+        #             if pileupread.alignment.seq[pileupread.qpos] == 'A':
+        #                 C_to_T += 1
+        #             elif pileupread.alignment.seq[pileupread.qpos] == 'G':
+        #                 not_changed += 1
+        #             else:
+        #                 C_to_other += 1
 
-            else:  # this is for the forward strand
-                if pileupread.qpos == 1 and  \
-                        pileupread.alignment.seq[pileupread.qpos] == 'G' \
-                        and (not pileupread.indel):
+        #     else:  # this is for the forward strand
+        #         if pileupread.qpos == 1 and  \
+        #                 pileupread.alignment.seq[pileupread.qpos] == 'G' \
+        #                 and (not pileupread.indel):
 
-                    tot_CG += 1
+        #             tot_CG += 1
 
-                    if pileupread.alignment.seq[pileupread.qpos-1] == 'T':
-                        C_to_T += 1
-                    if pileupread.alignment.seq[pileupread.qpos-1] == 'C':
-                        not_changed += 1
-                    if pileupread.alignment.seq[pileupread.qpos-1] in 'AG':
-                        C_to_other += 1
+        #             if pileupread.alignment.seq[pileupread.qpos-1] == 'T':
+        #                 C_to_T += 1
+        #             if pileupread.alignment.seq[pileupread.qpos-1] == 'C':
+        #                 not_changed += 1
+        #             if pileupread.alignment.seq[pileupread.qpos-1] in 'AG':
+        #                 C_to_other += 1
 
-        print(pileupcolumn.pos+1, not_changed,
-              C_to_T, C_to_other, tot_CG, file=f_output, sep='\t')
+        # print(pileupcolumn.pos+1, not_changed,
+        #       C_to_T, C_to_other, tot_CG, file=f_output, sep='\t')
 
     f_output.close()
     samfile.close()
