fe55de8d17aff3e5e274222cce47d75fdd3f41ec
hullibulli <kristianhanghoej@gmail.com>
2014-12-01 18:13:30 +0100

    plotting phasogram with smoothing

---
+++ R_analysis/plot_phaso.R
@@ -0,0 +1,14 @@
+require(ggplot2)
+require(zoo)
+files = list.files(pattern='*Saq*')
+pdf(file="plots/plot.pdf")
+Test <- lapply(files, function(x) {
+  a <- read.table(x)
+  b = a[a[,1]>=10 & a[,1]<=1020,]
+  # d = data.frame(x=b[,1],y=c)
+  d = data.frame(x=b[,1],y=b[,2])
+  d$smooth = rollmean(d$y,10,fill='extend')
+  p=ggplot(data=d,aes(x=x,y=smooth))
+  print(p+geom_bar(binwidth=1)+labs(title=x))
+})
+dev.off()
