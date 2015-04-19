1e9a9d8645f3d12b7cc802f6f333d3008e142d30
kristian hanghoej <kristianhanghoej@gmail.com>
2015-04-19 22:01:36 +0200

    epipaleomix is unit tested

--- code_structure/epipaleomix.py
+++ code_structure/epipaleomix.py
@@ -27,10 +27,6 @@ class MakefileError(RuntimeError):
     """Raised if a makefile is unreadable, or does not meet specifications."""
 
 
-class ConfigError(RuntimeError):
-    pass
-
-
 def parse_args(argv):
     ''' simpleparser '''
     usage_str = "%prog <command> [options] [makefiles]"
@@ -83,25 +79,31 @@ def checkbedfiles_ext(bedfiles):
             yield bedname, bedpath
 
 
+# def main_anal_to_run(opts):
+#     return {key: ANALYSES[key] for key, val in opts.iteritems() if
+#             key in ANALYSES.keys() and val['Enabled']}
+
+
 def main_anal_to_run(opts):
-    return {key: ANALYSES[key] for key, val in opts.iteritems() if
-            key in ANALYSES.keys() and val['Enabled']}
+    for analysis, options in opts.iteritems():
+        if analysis in ANALYSES.keys() and options['Enabled']:
+            yield analysis, ANALYSES[analysis]
 
 
-def calc_gccorrectionmodel(opts, prefix_opt, io_paths):
+def calc_gcmodel(opts, prefix_opt, io_paths):
     nodes = []
-    GC_resolution = opts['GCcorrect']['Resolution']
+    scale = opts['GCcorrect']['Resolution']
     suffix = io_paths['bamname']+'_GCcorrect'
     gc_dic = coerce_to_dic(prefix_opt, opts['BamInfo'], opts['GCcorrect'])
     rlmin, rlmax = gc_dic.pop('MapMinMaxReadLength', [56, 57])
 
-    for rl in xrange(rlmin, rlmax+1, GC_resolution):  # this is each readlength
+    for rl in xrange(rlmin, rlmax+1, scale):  # this is each readlength
         nodes.append(GccorrectNode(gc_dic, suffix, io_paths, rl))
     topnode = CreateGCModelNode(suffix, io_paths, subnodes=nodes)
 
     opts['NucleoMap']['--MaxReadLen'] = rlmax
-    GCmodf = (out for out in topnode.output_files if out.endswith('.txt'))
-    opts['BamInfo']['--GCmodel'] = GCmodf.next()
+    gcoutfile = (out for out in topnode.output_files if out.endswith('.txt'))
+    opts['BamInfo']['--GCmodel'] = gcoutfile.next()
     return [topnode]
 
 
@@ -115,26 +117,22 @@ def get_io_paths(config, bam_name):
 
 
 def make_gcnode(opts, prefix_opt, io_paths):
-    gc_bedfile_node = []
     if opts['GCcorrect'].get('Enabled', False):
-        gc_bedfile_node.extend(calc_gccorrectionmodel(opts, prefix_opt,
-                               io_paths))
-    return gc_bedfile_node
+        return calc_gcmodel(opts, prefix_opt, io_paths)
+    return []
 
 
 def run_filterbed(bedfiles, config, prefix_opt):
-    bedfilenode = []
     if bedfiles.get('EnabledFilter', False):
-        bedfilenode.extend(filter_bedfiles(bedfiles,
-                           config.temp_root,
-                           prefix_opt["--MappabilityPath"]))
-    return bedfilenode
+        return filter_bedfiles(bedfiles, config.temp_root,
+                               prefix_opt["--MappabilityPath"])
+    return []
 
 
-def run_analyses(aux_paths, analysis_options, BedInfo, mNode):
+def run_analyses(aux_paths, analysis_options, bedinfo, m_node):
     aux_python, aux_R = aux_paths
-    node = GeneralExecuteNode(aux_python, analysis_options, BedInfo,
-                              dependencies=mNode)
+    node = GeneralExecuteNode(aux_python, analysis_options, bedinfo,
+                              dependencies=m_node)
     curr_anal = analysis_options[1]
     if aux_R == '_':  # we have no plotting for writedepth
         return node
@@ -143,11 +141,11 @@ def run_analyses(aux_paths, analysis_options, BedInfo, mNode):
     return General_Plot_Node(aux_R, infile, curr_anal, dependencies=node)
 
 
-def makemetanode(gcnode, dependenNode, bamname):
+def make_metanode(depen_nodes, bamname):
     descrip_fmt = 'Metanode: {}: correction of GC and bedfile'.format
-    mNode = MetaNode(description=descrip_fmt(bamname),
-                     dependencies=dependenNode + gcnode)
-    return mNode
+    return MetaNode(description=descrip_fmt(bamname),
+                    dependencies=depen_nodes)
+
 
 ANALYSES = {'Phasogram': ['./tools/phasogram.py', './tools/phaso.R'],
             'WriteDepth': ['./tools/pileupdepth.py', '_'],
@@ -162,29 +160,27 @@ def run(config, makefiles):
     # this is not what we normally want:
     check_path(config.temp_root)
     logfile_template = time.strftime("Epiomix_pipe.%Y%m%d_%H%M%S_%%02i.log")
-
     pypeline.logger.initialize(config, logfile_template)
     logger = logging.getLogger(__name__)
     pipeline = Pypeline(config)
     topnodes = []
     for make in read_epiomix_makefile(makefiles):
-        dependenNode = []
+        filternode = []
         makefile = make.get('Makefile')
         prefix_opt = makefile.get('Prefixes')
         bedfiles = makefile.get('BedFiles')
-        dependenNode.extend(run_filterbed(bedfiles, config, prefix_opt))
+        filternode.extend(run_filterbed(bedfiles, config, prefix_opt))
         for bam_name, opts in makefile['BamInputs'].items():
             io_paths = get_io_paths(config, bam_name)
-            mNode = makemetanode(make_gcnode(opts, prefix_opt, io_paths),
-                                 dependenNode, bam_name)
-            bam_analyses = main_anal_to_run(opts)
+            m_node = make_metanode(make_gcnode(opts, prefix_opt, io_paths) +
+                                   filternode, bam_name)
             general_opts = coerce_to_dic(prefix_opt, opts["BamInfo"])
-            for BedInfo in checkbedfiles_ext(bedfiles):
-                for analysis, aux_paths in bam_analyses.iteritems():
+            for bedinfo in checkbedfiles_ext(bedfiles):
+                for analysis, aux_paths in main_anal_to_run(opts):
                     anal_opt = [io_paths['o_out'], analysis, GEN_OUTPUT_FMT,
                                 coerce_to_dic(general_opts, opts[analysis])]
                     topnodes.append(run_analyses(aux_paths, anal_opt,
-                                                 BedInfo, mNode))
+                                                 bedinfo, m_node))
     pipeline.add_nodes(topnodes)
     logger.info("Running BAM pipeline ...")
     pipeline.run(dry_run=config.dry_run, max_running=config.max_threads)
@@ -211,7 +207,7 @@ def main(argv):
         config, args = parse_args(argv)
         if args and args[0].startswith("dry"):
             config.dry_run = True
-    except ConfigError, error:
+    except RuntimeError, error:
         print_err(error)
         return 1
 
@@ -219,13 +215,11 @@ def main(argv):
     if (len(args) == 0) or (args[0] not in commands):
         _print_usage()
         return 1
-    # elif args[0] in ("mkfile", "makefile"):
-    #     return bam_mkfile.main(args[1:])
     elif not args[1:]:
         _print_usage()
         print_err("\nPlease specify at least one makefile!")
         return 1
-
+    # Note that we remove antother input before passing on to run func
     return run(config, args[1:])
 
 if __name__ == '__main__':
