f4d4d77b9493e0de0f50242b014a02c330ac38f9
kristian hanghoej <kristianhanghoej@gmail.com>
2015-05-07 21:38:25 +0200

    has to be implemented in each nodeerorr check. takes less than 1ms

--- epipipe/pypeline/tools/checkchromprefix.py
+++ epipipe/pypeline/tools/checkchromprefix.py
@@ -1,4 +1,4 @@
-import pysam, re
+import pysam, re, sys
 from pypeline.tools.commonutils import corr_chrom
 
 # bamfile input,
@@ -10,20 +10,30 @@ def unpack(chrom, *rest):
 def pa():
     pass
 
-def main(argv):
-    args, unknown = pa(argv)
-    samfile = pysam.Samfile(args.bam, 'rb')
+#def main(argv):
+def main():
+#    args, unknown = pa(argv)
+#    samfile = pysam.Samfile(args.bam, 'rb')
+    samfile = pysam.Samfile('saqqaq_chrom22.bam', 'rb')
     bamchrom = [dic['SN'] for dic in samfile.header['SQ']]
-    bedchroms = []
-    chromlast = ''
+
     
-    with open(args.bed, 'r') as fin:
+#    with open(args.bed, 'r') as fin:
+    with open('CTCF_human_2kb_rmoverlap.bed', 'r') as fin:
+        chromlast = ''
+        bedchroms = []
         for line in fin:
             chrom = unpack(*re.split(r'\s+', line.rstrip()))
             if chrom != chromlast:
-                bedchroms.append(corr_chrom(args.BamPrefix, chrom))
+#                bedchroms.append(corr_chrom(args.BamPrefix, chrom))
+                bedchroms.append(corr_chrom('', chrom))
                 chromlast = chrom
     checked = [(c in bamchrom) for c in bedchroms]
     assert all(checked), \
         ('chromosome: "%s" is/are not present in the bamfile' %
          ', '.join((bedchroms[idx] for idx, c in enumerate(checked) if not c)))
+
+
+if __name__ == '__main__':
+#    sys.exit(main(sys.argv[1:]))
+    sys.exit(main())
