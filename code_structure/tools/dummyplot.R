530395d7186e334a1504d332a7f5a37779e40686
krishang <kristianhanghoej@gmail.com>
2015-05-24 18:30:41 +0200

    done some cleaning up

--- code_structure/tools/dummyplot.R
+++
@@ -1,6 +0,0 @@
-args <- commandArgs(trailingOnly = TRUE)
-IN_path = args[1]
-outputplot = args[2]
-pdf(outputplot)
-plot(1, type="n", axes=F, xlab=sprintf("NODATA in %s",IN_path), ylab="")
-dev.off()
