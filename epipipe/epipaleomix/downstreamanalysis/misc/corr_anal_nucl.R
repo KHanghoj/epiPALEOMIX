96a43a9cd67c061b05dd28fbea7652d874709a76
krishang <kristianhanghoej@gmail.com>
2015-07-18 19:09:23 +0200

    corr_anal_nucl.R updated

--- epipipe/epipaleomix/downstreamanalysis/misc/corr_anal_nucl.R
+++ epipipe/epipaleomix/downstreamanalysis/misc/corr_anal_nucl.R
@@ -3,7 +3,7 @@ require(Hmisc)
 rdf <- function(f, col){
     print(getnames(f))
     df <- fread(sprintf('zcat %s',f),data.table=F)
-    df$nampos <- sprintf('%s_%s_%s',df[,1],df[,2],df[,3])
+    df$nampos <- sprintf('%s_%s',df[,1],df[,2])
     df <- df[,c(col,'nampos')]
     colnames(df) <- c(getnames(f), 'nampos')
     df
@@ -16,6 +16,7 @@ getnames <- function(f){
 files <- list.files(pattern='txt.gz',recursive=T)
 
 files <- files[grepl('_WriteDepth_CONSERVEDARRAY',files)]
+files <- files[!grepl('RISE',files)]
 
 mdf = Reduce(function(x, y) merge(x, y, by=c("nampos"), all=TRUE), lapply(files,rdf, col='depth'),accumulate=F)
 rcorr(as.matrix(mdf[2:ncol(mdf)]))
