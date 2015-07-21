6b2d39e7d93f2137de2fa74611ef5531107b7f85
krishang <kristianhanghoej@gmail.com>
2015-07-21 18:15:36 +0200

    made a script for logitregression

---
+++ epipipe/epipaleomix/downstreamanalysis/logitregression.R
@@ -0,0 +1,55 @@
+err.func <- function(X1, X2){
+    sum(sqrt((X1-X2)**2))/length(X1)
+}
+library(data.table)
+df <- fread('zcat bedcoord_MANYBASES_SNPsextra/AltaiNeanderthal_MethylMap_RRBSk1500_bedcoord.txt.gz',data.table=F)
+df$nampos <- sprintf('%s_%s',df[,1],df[,2]+750)
+tissue <- fread('../methylation/RRBS/onlyRRBSoverlappingPROM.bed', data.table=F)
+#tissue <- fread('../methylation/RRBS/onlyRRBSoverlappingGEBO.bed', data.table=F)
+
+tissue$nampos <- sprintf('%s_%s',tissue[,1],tissue[,2])
+
+mdf <- merge(df, tissue, by='nampos')
+mdfkeep = mdf[mdf$coverage>1000,]
+mdfkeep$moderndeaminated = round(with(mdfkeep, V4*V5),0)
+mdfkeep$V5bin= with(mdfkeep, cut(V5, breaks=c(-1,seq(0.1,1,0.1)),labels=1:10))
+
+
+
+glm.out.raw = glm(cbind(moderndeaminated, V4-moderndeaminated)~methylprop*deaminvariance*CpGsites*deaminvariance*nodeaminvariance,family=binomial(logit),data=mdfkeep[1:100000,])
+err.func(glm.out.raw$fitted.values, with(mdfkeep[1:100000,],moderndeaminated/V4))
+
+
+
+glm.out.raw.lm = lm((moderndeaminated/V4)~methylprop*deaminvariance*CpGsites*deaminvariance*nodeaminvariance,data=mdfkeep[1:100000,])
+err.func(glm.out.raw.lm$fitted.values, with(mdfkeep[1:100000,],moderndeaminated/V4))
+mdfkeep$V5bin= with(mdfkeep, cut(V5, breaks=c(-1,seq(0.1,1,0.1)),labels=1:10))
+
+anova(glm.out.raw,test="Chisq")
+
+
+
+glm.out.raw = glm(cbind(moderndeaminated, V4-moderndeaminated)~methylprop*deaminvariance*CpGsites*deaminvariance*nodeaminvariance,family=binomial(logit),data=mdfkeep[1:10000,])
+glm.out.raw.lm = lm((moderndeaminated/V4)~methylprop*deaminvariance*CpGsites*deaminvariance*nodeaminvariance,data=mdfkeep[1:10000,])
+glm.out.raw.simple = lm((moderndeaminated/V4)~methylprop,data=mdfkeep[1:10000,])
+
+d = sort(glm.out.raw$fitted.values)
+dsimple = sort(glm.out.raw.simple$fitted.values)
+dlm = sort(glm.out.raw.lm$fitted.values)
+
+with(mdfkeep[1:10000,], plot(d, sort(V5),col='green'))
+with(mdfkeep[1:10000,], points(dsimple, sort(V5),col='blue'))
+with(mdfkeep[1:10000,], points(dlm, sort(V5), col='red'))
+with(mdfkeep[1:10000,], points(sort(methylprop)/max(methylprop), sort(V5)))
+dev.off()
+
+d = (glm.out.raw$fitted.values-mean(glm.out.raw$fitted.values))/sd(glm.out.raw$fitted.values)
+l = (glm.out.raw.lm$fitted.values-mean(glm.out.raw.lm$fitted.values))/sd(glm.out.raw.lm$fitted.values)
+real = (mdfkeep[1:10000,'V5']-mean(mdfkeep[1:10000,'V5']))/sd(mdfkeep[1:10000,'V5'])
+raw = (mdfkeep$methylprop-mean(mdfkeep$methylprop))/sd(mdfkeep$methylprop)
+
+plot(d,real, col='green')
+points(l,real,col='red')
+points(real,real,col='black')
+points(raw[1:10000],real,col='blue')
+dev.off()
--- epipipe/epipaleomix/downstreamanalysis/misc/corr_anal_nucl.R
+++ epipipe/epipaleomix/downstreamanalysis/misc/corr_anal_nucl.R
@@ -1,5 +1,14 @@
 require(data.table)
 require(Hmisc)
+readdf <- function(f){
+    print(getnames(f))
+    df <- fread(spriBntf('zcat %s',f),data.table=F)
+    df$nampos <- sprintf('%s_%s',df[,1],df[,2])
+    df$nam <- getnames(f)
+    df
+}
+
+
 rdf <- function(f, col){
     print(getnames(f))
     df <- fread(sprintf('zcat %s',f),data.table=F)
@@ -8,6 +17,17 @@ rdf <- function(f, col){
     colnames(df) <- c(getnames(f), 'nampos')
     df
 }
+spectrumwrapper <- function(score){
+    ## takes a vector of scores as input
+    d <- spectrum(scores,method='pgram',plot=F)
+    data.frame(x=1/d$freq,
+               y=d$spec)
+}
+###
+###OR
+###
+####  spectrum(na.omit(mdf)[,2:ncol(mdf)],plot=F)  ## this works too
+
 getnames <- function(f){
     nam <- unlist(strsplit(f, '/'))
     unlist(strsplit(nam[length(nam)], '_'))[1]
@@ -17,9 +37,102 @@ files <- list.files(pattern='txt.gz',recursive=T)
 
 files <- files[grepl('_WriteDepth_CONSERVEDARRAY',files)]
 files <- files[!grepl('RISE',files)]
-
+dfs <- lapply(files,readdf)
 mdf = Reduce(function(x, y) merge(x, y, by=c("nampos"), all=TRUE), lapply(files,rdf, col='depth'),accumulate=F)
 rcorr(as.matrix(mdf[2:ncol(mdf)]))
 mdf = Reduce(function(x, y) merge(x, y, by=c("nampos"), all=TRUE), lapply(files,rdf, col='score'),accumulate=F)
 rcorr(as.matrix(mdf[2:ncol(mdf)]))
 
+
+
+
+
+## correlation and convolution:
+## http://www.cs.umd.edu/~djacobs/CMSC426/Convolution.pdf
+## https://en.wikipedia.org/wiki/Convolution
+
+
+mdfnaomit = naomit(mdf)
+
+filteredsample <- function(nam, mdf=mdfnaomit, window=window){
+    centered <- (mdfnaomit[,nam]-mean(mdfnaomit[,nam]))/sd(mdfnaomit[,nam])
+    ###filter(centered,window)
+    convolve(centered,window,type='filter')
+}
+
+centeredsaq <- (mdfnaomit[,c('Saqqaq')]-mean(mdfnaomit[,c('Saqqaq')]))/sd(mdfnaomit[,c('Saqqaq')])
+centeredabo <- (mdfnaomit[,c('Aborigin')]-mean(mdfnaomit[,c('Aborigin')]))/sd(mdfnaomit[,c('Aborigin')])
+
+
+window <- (c(seq(1,73),74,seq(73,1))) ## weighted filtering window weighing the same in the center
+windowones=rep(1,147)  ## this is not good. just for testing
+
+### Filter:
+valsaq <- convolve(centeredsaq,window,type='filter')
+valabo <- convolve(centeredabo,window,type='filter')
+### OR:
+valsaq <- filter(centeredsaq,window)  ## filter is the same as convolve(...,type='filter'). though it adds NA' (window/2) as beginning and end.
+valabo <- filter(centeredabo,window)
+
+valdeni <- filteredsample('Denisova')
+
+#co1 <- convolve(datsaq,databo)
+
+jump=5000
+for (idx in seq(0,length(valsaq)-jump,jump)){
+    dat = ccf(valabo[idx:(idx+jump)],valsaq[idx:(idx+jump)],plot=F)
+    l <- which(dat$acf==max(dat$acf))
+    print(c(round(max(dat$acf),1),dat$lag[l],idx, idx+jump))
+}
+    
+err.func <- function(p,X1, X2){
+    sum(sqrt((X1-X2+p[1])**2))
+}
+err.func <- function(X1, X2){
+    sum(sqrt((X1-X2)**2))/length(X1)
+}
+### this does not work
+## err.func <- function(p,X1, X2){
+##     cor(X1,X2+p[1])
+## }
+
+dat = ccf(valabo[1:10000],valsaq[1:10000],plot=F)
+dat = ccf(valabo,valsaq,plot=F)
+par <- c(0,0)
+optimx(par,err.func, X1=valsaq,X2=valabo)
+##optimx(par,err.func, X1=mdfnaomit[1:50000,c('Saqqaq')],X2=mdfnaomit[1:50000,c('Aborigin')])
+
+
+cor.test(mdfnaomit[,c('Aborigin')], mdfnaomit[,c('Saqqaq')])
+cor.test(valsaq,valabo)
+
+
+plot(valabo[1:2000])
+plot(mdfnaomit[73:2073,c('Aborigin')])
+dev.off()
+
+convolved = convolve(mdfnaomit[1:2000,c('Aborigin')],mdfnaomit[1:2000,c('Aborigin')],type='open')
+which(convolved==max(convolved))  # gives 2000 ## nothing to skew as same sample.
+convolved = convolve(mdfnaomit[1:500,c('Aborigin')],mdfnaomit[1:500,c('Saqqaq')],type='open')
+convolved = convolve(valabo[1:500],valsaq[1:500],type='open')
+which(convolved==max(convolved))  # gives 492. skew 8 bases
+
+convolved = convolve(mdfnaomit[1:500,c('Aborigin')],mdfnaomit[1:500,c('Saqqaq')],type='open')
+convolved = convolve(valabo[1:500],valsaq[1:500],type='open')
+which(convolved==max(convolved))  # gives 492. skew 8 bases
+
+co1 = convolve(mdfnaomit[1:300,c('Aborigin')],mdfnaomit[1:300,c('Saqqaq')],type='open')
+plot(co1)
+which(co1==max(co1))
+co2 = convolve(valabo[1:300],valsaq[1:300],type='open')
+plot(co2)
+which(co2==max(co2))
+dev.off()
+
+
+
+valsaq = convolve(mdfnaomit[,c('Saqqaq')],window,type='filter')
+valabo = convolve(mdfnaomit[,c('Denisova')],window,type='filter')
+plot(valsaq[1:500])
+plot(valabo[1:500])
+dev.off()
