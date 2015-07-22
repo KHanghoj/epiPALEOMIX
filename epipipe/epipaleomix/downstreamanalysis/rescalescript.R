1bb46c348840e4737a83aff99c4e23061cb1e6bf
krishang <kristianhanghoej@gmail.com>
2015-07-22 11:30:01 +0200

    rescalescript to find minimum and maxium of ms scores before Rs calculations

--- epipipe/epipaleomix/downstreamanalysis/rescalescript.R
+++ epipipe/epipaleomix/downstreamanalysis/rescalescript.R
@@ -2,32 +2,72 @@ err.func <- function(X1, X2){
     sum(sqrt((X1-X2)**2))/length(X1)
 }
 library(data.table)
-df <- fread('zcat bedcoord_MANYBASES_SNPsextra/AltaiNeanderthal_MethylMap_RRBSk1500_bedcoord.txt.gz',data.table=F)
+df <- fread('zcat /disk/ginzburg/data/krishang/methyl_rawdata/bedcoord_MANYBASES_SNPsextra/AltaiNeanderthal_MethylMap_RRBSk1500_bedcoord.txt.gz',data.table=F)
 df$nampos <- sprintf('%s_%s',df[,1],df[,2]+750)
-tissue <- fread('../methylation/RRBS/onlyRRBSoverlappingPROM.bed', data.table=F)
-#tissue <- fread('../methylation/RRBS/onlyRRBSoverlappingGEBO.bed', data.table=F)
-
+## tissue <- fread('../methylation/RRBS/onlyRRBSoverlappingPROM.bed', data.table=F)
+## tissue <- fread('../methylation/RRBS/onlyRRBSoverlappingGEBO.bed', data.table=F)
+tissue <- fread('/disk/ginzburg/data/krishang/methylation/RRBS/ucscdownfiltered_collapsed_new/Osteobl.bed', data.table=F)
 tissue$nampos <- sprintf('%s_%s',tissue[,1],tissue[,2])
 
 mdf <- merge(df, tissue, by='nampos')
-mdfkeep = mdf[mdf$coverage>100,]
-mdfkeep$moderndeaminated = round(with(mdfkeep, V4*V5),0)
-mdfkeep$V5bin= with(mdfkeep, cut(V5, breaks=c(-1,seq(0.1,1,0.1)),labels=1:10))
+mdfkeep <- mdf[mdf$coverage>100,]
+mdfkeep <- mdf[mdf$coverage>1000,]  ## this gives a more clear correlation between modern and ancient
+mdfkeep <- mdf[mdf$coverage>2000,] 
+
+mdfkeep$V5bin <- with(mdfkeep, cut(V5, breaks=c(-1,seq(0.1,1,0.1)),labels=1:10))
 
 fmodern <- function(bin,mdf){
     mean(mdf$V5[mdf$V5bin==bin])
 }
 par <- 1:10
-m = unlist(lapply(par,fmodern, mdf=mdfkeep))
+m <- unlist(lapply(par,fmodern, mdf=mdfkeep))
 
 fown <- function(bin,mdf){
     mean(mdf$methylprop[mdf$V5bin==bin])
 }
-o = unlist(lapply(par,fown, mdf=mdfkeep)) ## this is what we need for rescaling
-plot(m,o)
+o <- unlist(lapply(par,fown, mdf=mdfkeep)) ## this is what we need for rescaling
+rescaledf <- data.frame(modern=m, methylprop=o)
+plot(rescaledf, xlab='Moderndata 10bins', ylab='Ms score from same 10 bins')
 dev.off()
+rescalemodel <- lm(modern~methylprop,data=rescaledf)
+mdfkeep$methylrescaled <- predict(rescalemodel, data.frame(methylprop=mdfkeep$methylprop))
+
+mdfkeep$methylrescaled[mdfkeep$methylrescaled<m[1]] <- m[1]
+mdfkeep$methylrescaled[mdfkeep$methylrescaled>m[10]] <- m[10]
+
+summary(lm(V5~methylprop*deaminatedsites*coverage*CpGsites*deaminvariance*nodeaminvariance,data=mdfkeep))$r.squared
+dim(mdfkeep)
+summary(lm(V5~methylrescaled*deaminatedsites*coverage*CpGsites*deaminvariance*nodeaminvariance,data=mdfkeep))$r.squared
+
+
+
+summary(lm(V5~methylprop*deaminatedsites*coverage*CpGsites*deaminvariance*nodeaminvariance,data=mdfkeep))$r.squared
+[1] 0.7077669
+dim(mdfkeep)
+[1] 941800     18
+summary(lm(V5~methylrescaled*deaminatedsites*coverage*CpGsites*deaminvariance*nodeaminvariance,data=mdfkeep))$r.squared
+[1] 0.7211457
+
+> summary(lm(V5~methylprop*deaminatedsites*coverage*CpGsites*deaminvariance*nodeaminvariance,data=mdfkeep))$r.squared
+[1] 0.6819824
+> dim(mdfkeep)
+[1] 637049     18
+> summary(lm(V5~methylrescaled*deaminatedsites*coverage*CpGsites*deaminvariance*nodeaminvariance,data=mdfkeep))$r.squared
+[1] 0.7093277
+
+
+> summary(lm(V5~methylprop*deaminatedsites*coverage*CpGsites*deaminvariance*nodeaminvariance,data=mdfkeep))$r.squared
+[1] 0.7578274
+> dim(mdfkeep)
+[1] 25757    18
+> summary(lm(V5~methylrescaled*deaminatedsites*coverage*CpGsites*deaminvariance*nodeaminvariance,data=mdfkeep))$r.squared
+[1] 0.8014873
+
+mdfkeep$moderndeaminated <- round(with(mdfkeep, V4*V5),0)
 mdfkeep$methylpropbin = cut(mdfkeep$methylprop, breaks= c(-1, seq(0.0007,0.007,0.007/10)), labels=1:10)
 unlist(lapply(par,f, mdfkeep=mdfkeep))
+
+
 
 val <- mean(mdfkeep$V5[mdfkeep$V5bin==1])
 mdfkeep$methylprop[mdfkeep$V5<val] <- val
