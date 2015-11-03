08e21ce453d082168d3550ac670e3bb0857dee68
kristian (KEH) <kristianhanghoej@gmail.com>
2015-11-03 11:06:46 +0100

    updated pileupdepth to look like newnucleomap

--- epipaleomix/epipaleomix/tools/pileupdepth.py
+++ epipaleomix/epipaleomix/tools/pileupdepth.py
@@ -4,7 +4,7 @@ import sys
 import pysam
 import argparse
 import gzip
-from collections import deque
+from collections import deque, namedtuple
 from os.path import exists, splitext
 from shutil import move
 from epipaleomix.tools.commonutils import read_bed, \
@@ -15,41 +15,46 @@ class Write_Depth(GC_correction):
     """docstring for Write_Depth"""
     def __init__(self, arg):
         self.arg = arg
+        self._create_constants()
+        GC_correction.__init__(self)
+        self._f_output = None
+        self._deq_depth = deque(self._DEQ_LEN_ZEROS, maxlen=self._DEQ_LEN)
+        self._deq_app = self._deq_depth.append
+        self._deq_popl = self._deq_depth.popleft        
+        self._mainlist = []
+        self._mainlapp = self._mainlist.append
+        self._outputlist = []
+        self._calltuple = namedtuple('call', 'pos depth score')
+        self._fmt = '{}\t{}\t{}\t{}\t{bedcoord}\n'
+        self._makeoutputfile()
+
+    def _create_constants(self):
         self._SIZE = self.arg.SIZE
         self._OFFSET, self._NEIGHBOR = self.arg.OFFSET, self.arg.FLANKS
         self._POSITION_OFFSET = self._OFFSET+self._NEIGHBOR
         self._TOTAL_WIN_LENGTH = self._SIZE+(2*self._OFFSET)+(2*self._NEIGHBOR)
-        self._HALFWINDOW =  (self._TOTAL_WIN_LENGTH-1)/2  # 110
+        self._HALFWINDOW = int((self._TOTAL_WIN_LENGTH-1)/2)  ## +1
         self._CENTERINDEX = (self._SIZE-1)/2
+        self._DEQ_LEN = int(self.arg.DequeLength+50)
+        self._DEQ_LEN_ZEROS = [0]*self._DEQ_LEN
+        self._HALFWINDOW_ZEROS = [0]*self._HALFWINDOW
+        self._MAX_JUMP = self._DEQ_LEN*4
         self._SPACERMEAN = float(self._NEIGHBOR+self._NEIGHBOR)
-        GC_correction.__init__(self)
-        self._f_output = None
-        self._seq_len = int(self._TOTAL_WIN_LENGTH*2)  # maybe times 4 if small nucl
-        self._zeros = [0]*self._seq_len
-        self._last_pos = -self._seq_len
-        self._deq_depth = deque(maxlen=self._seq_len)
-        self._mainlist = list()
-        self._fmt = '{}\t{}\t{}\t{}\t{}\n'
-        self._makeoutputfile()
 
     def update_depth(self, record):
         if not self._last_ini:
             self._last_ini = record.pos+1
             self._last_pos = record.pos
         _jump_idx = record.pos - self._last_pos
-        if _jump_idx:
-            if _jump_idx >= self._seq_len:
-                self._mainlist.extend(self._deq_depth)
-                self._call_depth_scores()
-                self._mainlist = self._zeros[:self._HALFWINDOW]
-                self._last_ini = record.pos+1-self._HALFWINDOW
-                self._deq_depth = deque(self._zeros, maxlen=self._seq_len)
-                _jump_idx = 0
-            else:
-                while _jump_idx:
-                    self._mainlist.append(self._deq_depth.popleft())
-                    self._deq_depth.append(0)
-                    _jump_idx -= 1
+        if _jump_idx >= self._MAX_JUMP:
+            self.call_depths()
+            self.reset_inregion(record)
+            _jump_idx = 0
+        while _jump_idx:
+            self._mainlapp(self._deq_popl())
+            self._deq_app(0)
+            _jump_idx -= 1
+
         self._last_pos = record.pos
 
         if record.is_reverse:
@@ -64,31 +69,37 @@ class Write_Depth(GC_correction):
                 _jump_idx += count
             elif cigar in (2, 3, 6):
                 _jump_idx += count
+                
+    def call_depths(self):
+        if sum(self._mainlist) or sum(self._deq_depth):
+            self._mainlist.extend(self._deq_depth)
+            self._mainlist.extend(self._HALFWINDOW_ZEROS)
+            self._call_depth_scores()
 
     def _depthwindows(self):
         n = len(self._mainlist)
         for idx in xrange(0, n-self._TOTAL_WIN_LENGTH+1):
-            if self._mainlist[idx+self._HALFWINDOW]:  ## check that center is not zero depth
+            if self._mainlist[idx+self._HALFWINDOW]:
                 yield idx, self._mainlist[idx:(idx+self._TOTAL_WIN_LENGTH)]
-                
-    def calcscore(self, window):
+
+    def _calcscore(self, window):
         center = window[self._HALFWINDOW]
         spacer = (sum(window[:self._NEIGHBOR])+sum(window[-self._NEIGHBOR:]))/self._SPACERMEAN
+        # spacer = spacer if spacer > 1 else 1
         return (center, center-spacer)
                 
     def _call_depth_scores(self):
         for idx, window in self._depthwindows():
-            # get to the center of the window
             pos = self._last_ini+idx+self._HALFWINDOW
             if pos >= self.start and pos <= self.end:
-                depth, score = self.calcscore(window)
-                self.f_output.write(self._fmt.format(self.chrom, pos,
-                                                     depth, score,
-                                                     self.bedcoord))
+                depth, score = self._calcscore(window)
+                self._outputlist.append(self._calltuple(pos, depth, score))
 
-    def call_final_depth_scores(self):
-        self._mainlist.extend(self._deq_depth)
-        self._call_depth_scores()
+    def writetofile(self):
+        for dat in self._outputlist:
+            self.f_output.write(self._fmt.format(self.chrom, *dat,
+                                                 bedcoord=self.bedcoord))
+        self._outputlist = [] 
 
     def _makeoutputfile(self):
         ''' want to write to file every chrom, to keep scalablility'''
@@ -97,13 +108,23 @@ class Write_Depth(GC_correction):
             move(self.arg.outputfile, pathname+'_old'+extens)
         self.f_output = gzip.open(self.arg.outputfile, 'ab')
  
-    def reset_deques(self, chrom, start, end, bedcoord):
-        self.chrom, self.start, self.end = chrom, start, end
-        self._deq_depth = deque(self._zeros, maxlen=self._seq_len)
-        self._last_ini = None
-        self._mainlist = []
-        self.bedcoord = bedcoord
+    def reset_inregion(self, record):
+        self._deq_depth.clear()
+        self._deq_depth.extend(self._DEQ_LEN_ZEROS)
+        del self._mainlist [:]
+        self._mainlist.extend(self._HALFWINDOW_ZEROS)
+        self._last_ini = record.pos+1-self._HALFWINDOW
 
+    def reset_deques(self, chrom, start, end, bedcoord):
+        self._deq_depth.clear()
+        self._deq_depth.extend(self._DEQ_LEN_ZEROS)
+        del self._mainlist [:] # everytime new bed is called. flanks are made automatically. see run func
+        self._last_ini = 0
+        self.start, self.end, self.chrom = start, end, chrom
+        self.bedcoord = bedcoord
+        self._outputlist = []
+
+        
     def closefile(self):
         self.f_output.close()
         try:
@@ -140,7 +161,8 @@ def run(args):
             if record.mapq < args.MinMappingQuality or record.is_unmapped:
                 continue  # do not analyze low quality records
             Corr_Depth.update_depth(record)
-        Corr_Depth.call_final_depth_scores()
+        Corr_Depth.call_depths()
+        Corr_Depth.writetofile()
     Corr_Depth.closefile()
     return 0
 
