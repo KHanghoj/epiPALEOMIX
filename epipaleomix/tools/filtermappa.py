51ce634f59284f09af4142d6b4e4f3603240c0d6
kristian (KEH) <kristianhanghoej@gmail.com>
2015-11-13 19:02:10 +0100

    removed -V for sort as it does not work on standard Unix (mac) computers

--- epipaleomix/tools/filtermappa.py
+++ epipaleomix/tools/filtermappa.py
@@ -13,11 +13,13 @@ def parse_args(argv):
 def main(argv):
     args, unknown = parse_args(argv)
     with open(args.inputfile, 'r') as infile:
+        count = 0
         for line in infile:
             inputline = line.rstrip('\n').split('\t')
             if float(inputline[-1]) >= args.mappauniqueness:
+                count += 1
                 sys.stdout.write("\t".join(inputline[:3])+'\n') ## write coordinates only
-                ## sys.stdout.write("\t".join(inputline)+'\n')
+        assert count, "No Regions in the genome displayed complexity levels equal to or higher than %s\n Recommended uniqueness score 0.75 - 0.9" % (args.mappauniqueness)
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
--- epipaleomix/tools/updatebedcoord.py
+++ epipaleomix/tools/updatebedcoord.py
@@ -2,6 +2,13 @@ import sys, re
 import fileinput
 from collections import namedtuple
 
+"""
+takes a filter-by-uniqueness mappability file intersected with current bed file.
+column 1-3 is the unique coordinates
+column 4-6 is the old bed coordinates. They will still be used as original bedcoordinates as they are unique for the input.
+6+ is leftover from the original input.
+"""
+
 OUTFMT = '{}\t{}\t{}\t{}\n'.format
 BEDCOORD = '{}_{}_{}'.format
 lasttup, intup = (), ()
@@ -10,7 +17,8 @@ NA_TUP = namedtuple('row', 'c s e bedc')
 
 def unpack(c,s,e,*rest):
     return NA_TUP(c, int(s), int(e), BEDCOORD(*rest[:3]))
-    
+
+
 for line in fileinput.input():
     intup = unpack(*re.split('\s+', line.rstrip('\n')))
     if not lasttup:
@@ -28,3 +36,5 @@ for line in fileinput.input():
     lasttup = intup
 if intup:
     sys.stdout.write(OUTFMT(intup.c, start, intup.e, intup.bedc))
+else:
+    raise RuntimeError("No Bed Regions overlap the uniquely mappable regions")
