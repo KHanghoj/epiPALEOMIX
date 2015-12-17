bd601fb79d709146b25c23ce3b1c7d1f60fb9480
kristian (KEH) <kristianhanghoej@gmail.com>
2015-12-17 17:45:14 +0100

    fixed a couple of bugs in methylmap, kept the original

--- epiomix/tools/methylmap.py
+++ epiomix/tools/methylmap.py
@@ -34,32 +34,49 @@ class Methyl_Level(object):
     """docstring for Methyl_Level"""
     def __init__(self, arg):
         self.arg = arg
-        self._ReadBases = self.arg.ReadBases
+        self._ReadBases = self.arg.ReadBases+1
         self._fasta = Cache(self.arg.FastaPath)
         self.dic_pos = defaultdict(lambda: defaultdict(int))
         self.pat = re.compile('CG')
         self.rows = list()
+
         self.rowsapp = self.rows.append
         self.na_tup = namedtuple('row', ('pos top lower'))
         self._makeoutputfile()
         self._row_size = int(1e6)
         self._updatefunc = self._choosefunc(self.arg.Primes.lower())
-        self.forw_five = self._createargdict(_FORW_STRAND_BASES, 0, self.arg.SkipFivePrime, CONV_FORW)
-        self.forw_three = self._createargdict(_FORW_STRAND_BASES, 0, self.arg.SkipThreePrime, CONV_FORW)
-        self.rev_five = self._createargdict(_REV_STRAND_BASES, 1, self.arg.SkipFivePrime, CONV_REV)
-        self.rev_three = self._createargdict(_REV_STRAND_BASES, 1, self.arg.SkipThreePrime, CONV_REV)
+        self.forw_five = self._createargdict(_FORW_STRAND_BASES, 0,
+                                             self.arg.SkipFivePrime,
+                                             CONV_FORW)
+        self.forw_three = self._createargdict(_FORW_STRAND_BASES, 0,
+                                              self.arg.SkipThreePrime,
+                                              CONV_FORW)
+        self.rev_five = self._createargdict(_REV_STRAND_BASES, 1,
+                                            self.arg.SkipFivePrime,
+                                            CONV_REV)
+        self.rev_three = self._createargdict(_REV_STRAND_BASES, 1,
+                                             self.arg.SkipThreePrime,
+                                             CONV_REV)
+        # This is all the addons
+        self._count_pos_strand = [{x: 0 for x in _FORW_STRAND_BASES}
+                                  for base in xrange(self._ReadBases)]
+        self._count_neg_strand = [{x: 0 for x in _REV_STRAND_BASES}
+                                  for base in xrange(self._ReadBases)]
+        self._tempReadbase = self._ReadBases
 
-        # in a sam/bam file everything is plus strand oriented.even sequences, cigar, EVERYTHING
+        # in a sam/bam file everything is plus
+        # strand oriented.even sequences, cigar, EVERYTHING
         # cig_idx -1 returns last cigar of the sequence from a positive strand
         # perspective. so [-1] can be the 5' end of a reverse read.
 
     def _createargdict(self, bases, basep, skip, convdict):
-        return {'inbases': bases, 'basepos': basep, 'skip':  skip, 'conv': convdict}
-        
+        return {'inbases': bases, 'basepos': basep,
+                'skip':  skip, 'conv': convdict}
+
     def _choosefunc(self, prims):
-        dic={'both': self._both, 'five': self._only_5, 'three': self._only_3}
+        dic = {'both': self._both, 'five': self._only_5, 'three': self._only_3}
         return dic[prims]
-     
+
     def _only_5(self):
         if self.record.is_reverse:
             self._rightpart(**self.rev_five)
@@ -74,7 +91,7 @@ class Methyl_Level(object):
 
     def _both(self):
         # both is only for single strand libraries as the 3' overhangs
-        # are remove in double strand library preparation.
+        # are remove in double strand library construction
         if self.record.is_reverse:
             self._rightpart(**self.rev_five)
             self._leftpart(**self.rev_three)
@@ -82,20 +99,13 @@ class Methyl_Level(object):
             self._leftpart(**self.forw_five)
             self._rightpart(**self.forw_three)
 
-    def reset_dict(self, chrom, start, end, bedcoord):
-        self.dic_pos = defaultdict(lambda: defaultdict(int))
-        self.start, self.end, self.chrom = start, end, chrom
-        self.bedcoord = bedcoord
-        del self.rows[:]  # clear all data
-        self.last_end = 0
-
-    def _getindexes(self, bases_str):
-        ''' returns the 0-based indeces of fasta read'''
-        return (m.start() for m in self.pat.finditer(bases_str))
-
     def update(self, record):
         self.record = record
-        if self.last_end < self.record.pos:  # if new read is not overlapping previous
+        if self.record.alen < self._ReadBases:
+            self._tempReadbase = self._ReadBases
+            self._ReadBases = self.record.alen
+        # if new read is not overlapping previous
+        if self.last_end < self.record.pos:
             self._call_ms()
             self.dic_pos.clear()
             if len(self.rows) > self._row_size:
@@ -103,44 +113,113 @@ class Methyl_Level(object):
                 del self.rows[:]  # clear all data
         self._updatefunc()
         self.last_end = self.record.aend+1000  # to avoid overlapping sites.
+        self._ReadBases = self._tempReadbase
 
     def _call_ms(self):
         for pos, basescore in sorted(self.dic_pos.iteritems()):
-            if pos >= self.start and pos <= self.end:  # make sure overlapping genomic sites do not get counted twice
+            # make sure overlapping genomic sites do not get counted twice
+            if pos >= self.start and pos <= self.end:
                 top = basescore.get('T', 0)
                 low = top+basescore.get('C', 0)
                 self.rowsapp(self.na_tup(pos, top, low))
-                
-    def call_final_ms(self):
-        self._call_ms()
-        self._writetofile()
-        del self.rows[:]  # clear all data
+
+    def _generate_cigar_set(self, record):
+        ''' returns a set with the positions in a
+        read that match/mismatch the reference '''
+        readmatch = set()
+        _jump_idx = 0
+        cigar = record.cigar[::-1] if record.is_reverse else record.cigar
+        addon = record.alen - 1 if record.is_reverse else 0
+        for (cigar, count) in cigar:
+            if cigar in (0, 7, 8):
+                for idx in xrange(_jump_idx, _jump_idx + count):
+                    readmatch.add(abs(addon-idx))
+                    if idx > self._ReadBases:
+                        return readmatch
+                _jump_idx += count
+            elif cigar in (2, 3, 6):
+                _jump_idx += count
+        return readmatch
 
     def _prep(self, curr_pos, skip=0):
-        ## skip is only used in right side functions
+        # skip is only used in right side functions
         fast_string = self._fasta.fetch_string(self.chrom, curr_pos,
                                                self._ReadBases-skip)
-        for fast_idx in self._getindexes(fast_string):
-            yield fast_idx, self._ReadBases - fast_idx
+        return [m.start() for m in self.pat.finditer(fast_string)]
+
+    def _alltheread():
+        ''' returns CpG hits across the entire read '''
+        pass
 
     def _rightpart(self, inbases, basepos, skip, conv):
         curr_pos = self.record.aend-self._ReadBases
-        cigar_op, cigar_len = self.record.cigar[-1]
-        bases = self.record.seq[-self._ReadBases:]
-        for fast_idx, inverse_idx in self._prep(curr_pos, skip):
-            if (cigar_op == 0 and cigar_len >= inverse_idx and
-                bases[fast_idx:fast_idx+2] in inbases):
-                self.dic_pos[curr_pos+fast_idx+1][conv[bases[fast_idx+basepos]]] += 1
+        cpg_indexes = self._prep(curr_pos, skip)
+        if cpg_indexes:
+            cigarmatch = self._generate_cigar_set(self.record)
+            bases = self.record.seq[-self._ReadBases:]
+
+            for fast_idx in cpg_indexes:
+                inverse_idx = self._ReadBases-fast_idx
+                curr_cpgcheck = bases[fast_idx:fast_idx+2]
+                if ((self.record.alen-inverse_idx) in cigarmatch and
+                    (self.record.alen-inverse_idx+1) in cigarmatch and
+                        curr_cpgcheck in inbases):
+                    # TODO:
+                    # if looking at both ends of reads,
+                    # we need to change the index accordingly.
+                    if self.record.is_reverse:
+                        (self._count_neg_strand[inverse_idx-1]
+                         [curr_cpgcheck]) += 1
+                    else:
+                        (self._count_pos_strand[fast_idx]
+                         [curr_cpgcheck]) += 1
+
+                    (self.dic_pos[curr_pos+fast_idx+1]
+                     [conv[bases[fast_idx+basepos]]]) += 1
 
     def _leftpart(self, inbases, basepos, skip, conv):
         curr_pos = self.record.pos
-        cigar_op, cigar_len = self.record.cigar[0]
-        bases = self.record.seq[:self._ReadBases]
-        for fast_idx, _ in self._prep(curr_pos):
-            if (fast_idx >= skip and cigar_op == 0 and
-                cigar_len >= fast_idx+2 and
-                bases[fast_idx:fast_idx+2] in inbases):
-                self.dic_pos[curr_pos+fast_idx+1][conv[bases[fast_idx+basepos]]] += 1            
+        cpg_indexes = self._prep(curr_pos)
+        if cpg_indexes:
+            cigarmatch = self._generate_cigar_set(self.record)
+            bases = self.record.seq[:self._ReadBases]
+            for fast_idx in cpg_indexes:
+                inverse_idx = self._ReadBases-fast_idx
+                curr_cpgcheck = bases[fast_idx:fast_idx+2]
+                if (fast_idx >= skip and fast_idx in cigarmatch and
+                    fast_idx+1 in cigarmatch and
+                        curr_cpgcheck in inbases):
+
+                    if self.record.is_reverse:
+                        (self._count_neg_strand[inverse_idx-1]
+                         [curr_cpgcheck]) += 1
+                    else:
+                        (self._count_pos_strand[fast_idx]
+                         [curr_cpgcheck]) += 1
+
+                    (self.dic_pos[curr_pos+fast_idx+1]
+                     [conv[bases[fast_idx+basepos]]]) += 1
+
+    def _writetofile(self):
+        ''' every row contain chrom, genomicpos, top, lower, bedcoord'''
+        for row in self.rows:
+            self.f_output.write(self.fmt(r=row,
+                                         chrom=self.chrom,
+                                         bed=self.bedcoord))
+
+    def _print_position_counts(self):
+        ''' return summary for each ReadBase analyzed '''
+        sys.stdout.write("Strand\tPLUS\tPLUS\tNEGA\tNEGA\n")
+        sys.stdout.write("CpGPos\tTG\tCG\tCA\tCG\n")
+
+        for base in xrange(self._ReadBases):
+            curr_pos = self._count_pos_strand[base]
+            curr_neg = self._count_neg_strand[base]
+            sys.stdout.write('{}\t{}\t{}\t{}\t{}\n'.format(base+1,
+                                                           curr_pos["TG"],
+                                                           curr_pos["CG"],
+                                                           curr_neg["CA"],
+                                                           curr_neg["CG"]))
 
     def _makeoutputfile(self):
         ''' want to write to file every chrom, to keep scalablility'''
@@ -150,49 +229,66 @@ class Methyl_Level(object):
         self.f_output = gzip.open(self.arg.outputfile, 'ab')
         self.fmt = "{chrom}\t{r.pos}\t{r.top}\t{r.lower}\t{bed}\n".format
 
-    def _writetofile(self):
-        ''' every row contain chrom, genomicpos, top, lower, bedcoord'''
-        for row in self.rows:
-            self.f_output.write(self.fmt(r=row,
-                                         chrom=self.chrom,
-                                         bed=self.bedcoord))
+    def call_final_ms(self):
+        self._call_ms()
+        self._writetofile()
+        del self.rows[:]  # clear all data
+
+    def reset_dict(self, chrom, start, end, bedcoord):
+        self.dic_pos = defaultdict(lambda: defaultdict(int))
+        self.start, self.end, self.chrom = start, end, chrom
+        self.bedcoord = bedcoord
+        del self.rows[:]  # clear all data
+        self.last_end = 0
 
     def closefiles(self):
+        self._print_position_counts()
         self.f_output.close()
         self._fasta.closefile()
 
+
 def parse_helper(s):
     return s.lower()
-        
+
+
 def parse_args(argv):
     ''' docstring '''
     parser = argparse.ArgumentParser()
     parser.add_argument('bam', help="..", type=str)
     parser.add_argument('bed', help="..", type=str)
     parser.add_argument('outputfile', help='..', type=str)
-    parser.add_argument('--FastaPath', help="FastaPath", type=str)
-    parser.add_argument('--ReadBases', help="..", type=int, default=2)
-    parser.add_argument('--MinMappingQuality', help="..", type=int, default=20)
-    parser.add_argument('--MinAlignmentLength', help="..", type=int, default=25)
-    parser.add_argument('--Primes', help="..", type=parse_helper, default='five',
+    parser.add_argument('--FastaPath', help="FastaPath: %(default)s", type=str)
+    parser.add_argument('--ReadBases', help="%(default)d", type=int, default=2)
+    parser.add_argument('--MinMappingQuality', help="%(default)d",
+                        type=int, default=20)
+    parser.add_argument('--MinAlignmentLength', help="%(default)d",
+                        type=int, default=25)
+    parser.add_argument('--Primes', help="%(default)s",
+                        type=parse_helper, default='five',
                         choices=['both', 'five', 'three'])
-    parser.add_argument('--SkipThreePrime', help="..", type=int, default=0)
-    parser.add_argument('--SkipFivePrime', help="..", type=int, default=0)
+    parser.add_argument('--SkipThreePrime', help="%(default)d",
+                        type=int, default=0)
+    parser.add_argument('--SkipFivePrime', help="%(default)d",
+                        type=int, default=0)
     return parser.parse_known_args(argv)
 
 
 def run(args):
-    assert (args.ReadBases > args.SkipThreePrime and
-            args.ReadBases > args.SkipFivePrime), ("--ReadBases '{}' must be higher"
-    " than both --SkipThreePrime '{}' and SkipFivePrime '{}'".format(args.ReadBases,
-                                                                     args.SkipThreePrime,
-                                                                     args.SkipFivePrime))
+    assert (args.ReadBases > args.SkipThreePrime or
+            args.ReadBases > args.SkipFivePrime), \
+        ("--ReadBases '{}' must be higher than both"
+         " --SkipThreePrime '{}' and SkipFivePrime '{}'".format(
+             args.ReadBases,
+             args.SkipThreePrime,
+             args.SkipFivePrime))
     samfile = pysam.AlignmentFile(args.bam, "rb")
     Met_Score = Methyl_Level(args)
     for chrom, start, end, bedcoord in read_bed(args):
         Met_Score.reset_dict(chrom, start, end, bedcoord)
         for record in samfile.fetch(chrom, start, end):
-            if record.mapq < args.MinMappingQuality or record.is_unmapped or record.alen < args.MinAlignmentLength:
+            if (record.mapq < args.MinMappingQuality or
+                record.is_unmapped or
+                    record.alen < args.MinAlignmentLength):
                 continue  # do not analyze low quality records
             Met_Score.update(record)
         Met_Score.call_final_ms()
